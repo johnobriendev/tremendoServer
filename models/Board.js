db75ad4f9c87b74cc857f544f896bf248bd7b287
John O'Brien <johnobrien.dev@gmail.com>
2024-08-13 16:22:41 -0400

    create board model

---
+++ models/Board.js
@@ -0,0 +1,13 @@
+// models/Board.js
+const mongoose = require('mongoose');
+
+const boardSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, default: '' },
+  isPrivate: { type: Boolean, default: false },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+}, { timestamps: true });
+
+const Board = mongoose.model('Board', boardSchema);
+
+module.exports = Board;

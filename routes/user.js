a544e37be0148d4cb24cd2f50d936c772eb503b1
John O'Brien <johnobrien.dev@gmail.com>
2024-09-16 09:53:59 -0400

    add limiter middleware to register route

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/userController');
 const { authenticateJWT } = require('../middleware/auth');
+const registerLimiter = require('../middleware/rateLimiter');
 
 
-router.post('/register', userController.registerUser);
+router.post('/register', registerLimiter, userController.registerUser);
 router.post('/login', userController.loginUser);
 router.post('/logout', userController.logoutUser);
 

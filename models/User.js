c80d08ef4108a6620949c8f3d804e3e2fbb7e8f9
John O'Brien <johnobrien.dev@gmail.com>
2024-09-17 16:35:05 -0400

    update user schema with verification

--- models/User.js
+++ models/User.js
@@ -4,6 +4,9 @@ const userSchema = new mongoose.Schema({
   name: { type: String, default: '' }, 
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  isVerified: { type: Boolean, default: false },
+  verificationToken: { type: String },
+  verificationTokenExpires: { type: Date },
 });
 
 const User = mongoose.model('User', userSchema);

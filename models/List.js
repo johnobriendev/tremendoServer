df8f58e49710bb674b9003e70553d590e22c7c20
John O'Brien <johnobrien.dev@gmail.com>
2024-09-26 14:18:06 -0400

    update ListModel to include color

--- models/List.js
+++ models/List.js
@@ -4,6 +4,7 @@ const listSchema = new mongoose.Schema({
   name: { type: String, required: true },
   boardId: { type: mongoose.Schema.Types.ObjectId, ref: 'Board', required: true },
   position: { type: Number, required: true }, 
+  color: { type: String, default: null },
 }, {
   timestamps: true,
 });

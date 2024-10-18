beb86b99de705d1ee3ac18c5e2d648c6e33187ec
John O'Brien <johnobrien.dev@gmail.com>
2024-10-18 08:50:46 -0400

    adjust cors options

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const passport = require('passport');
 const compression = require('compression');

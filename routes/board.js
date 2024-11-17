2ba82e6d8296d157be70bdf0c03afad92747613a
John O'Brien <johnobrien.dev@gmail.com>
2024-11-17 11:59:57 -0500

    clean up board of old comments

--- routes/board.js
+++ routes/board.js
@@ -5,19 +5,14 @@ const { authenticateJWT } = require('../middleware/auth');
 
 const router = express.Router();
 
-// Get all boards
 router.get('/', authenticateJWT, boardController.getBoards);
 
-// Create a new board
 router.post('/', authenticateJWT, boardController.createBoard);
 
-// Get a specific board by ID
 router.get('/:id', authenticateJWT, boardController.getBoardById);
 
-// Update a specific board
 router.put('/:id', authenticateJWT, boardController.updateBoard);
 
-// Delete a specific board
 router.delete('/:id', authenticateJWT, boardController.deleteBoard);
 
 module.exports = router;

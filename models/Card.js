9b43299afaaa8129c2442aa5f423443a2c80a0e0
John O'Brien <johnobrien.dev@gmail.com>
2024-12-31 14:37:15 -0500

    add encrypt and decrypt to list and card models

--- models/Card.js
+++ models/Card.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const { encrypt, decrypt } = require('../utils/encryption');
+
 
 const commentSchema = new mongoose.Schema({
-  text: { type: String, required: true },
+  text: { type: String, required: true, set: encrypt, get: decrypt },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   createdAt: { type: Date, default: Date.now },
 });
@@ -9,12 +11,17 @@ const commentSchema = new mongoose.Schema({
 const cardSchema = new mongoose.Schema({
   boardId: { type: mongoose.Schema.Types.ObjectId, ref: 'Board', required: true },
   listId: { type: mongoose.Schema.Types.ObjectId, ref: 'List', required: true },
-  name: { type: String, required: true },
-  description: { type: String },
+  name: { type: String, required: true, set: encrypt, get: decrypt },
+  description: { type: String, set: encrypt, get: decrypt },
   position: { type: Number, required: true },
   comments: [commentSchema],
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
+},
+{
+  timestamps: true,
+  toJSON: { getters: true },
+  toObject: { getters: true }
 });
 
 const Card = mongoose.model('Card', cardSchema);
--- models/List.js
+++ models/List.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
+const { encrypt, decrypt } = require('../utils/encryption');
+
 
 const listSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, set: encrypt, get: decrypt  },
   boardId: { type: mongoose.Schema.Types.ObjectId, ref: 'Board', required: true },
   position: { type: Number, required: true }, 
   color: { type: String, default: null },
 }, {
   timestamps: true,
+  toJSON: { getters: true },
+  toObject: { getters: true }
 });
 
 const List = mongoose.model('List', listSchema);

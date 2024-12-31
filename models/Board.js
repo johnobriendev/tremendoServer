8b1d85be7f83f59efc12e8b63bd3a7ebd7108a65
John O'Brien <johnobrien.dev@gmail.com>
2024-12-31 14:18:37 -0500

    add encrypt and decrypt to board model

--- models/Board.js
+++ models/Board.js
@@ -1,14 +1,16 @@
 // models/Board.js
 const mongoose = require('mongoose');
+const { encrypt, decrypt } = require('../utils/encryption');
+
 
 const boardSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, default: '' },
+  name: { type: String, required: true, set: encrypt, get: decrypt },
+  description: { type: String, default: '', set: encrypt, get: decrypt },
   isPrivate: { type: Boolean, default: true},
   owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   collaborators: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   backgroundColor: { type: String, default: '#ffffff' },
-}, { timestamps: true });
+}, { timestamps: true, toJSON: { getters: true }, toObject: { getters: true } });
 
 const Board = mongoose.model('Board', boardSchema);
 
